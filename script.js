5b642ad394a0bbff944ec927f72c2dd6ace48f8d
barni246 <barnabas.gonda@googlemai.com>
2024-04-08 19:49:28 +0200

    clean code comment

--- script.js
+++ script.js
@@ -5,61 +5,11 @@ function allowDrop(ev) {
   ev.preventDefault();
 }
 
+
 function drag(ev) {
   ev.dataTransfer.setData("text", ev.target.id);
   //console.log('dragging elemennt id:', ev.target.id)
 }
-
-// function drop(ev) {
-//     ev.preventDefault();
-//     const data = ev.dataTransfer.getData("text");
-//     const draggedElement = document.getElementById(data);
-//     console.log('data', data);
-//     const dropZone = ev.target.id;
-//     console.log('dropzone id: ',dropZone);
-//     const dragElements = Array.from(document.querySelectorAll('.drag')).map(element => element.id);
-//     const taskId = data;
-//     const task = tasks.find(task => task.id === parseInt(taskId));
-//     console.log('task',task);
-//     console.log('task column: ',task.column);
-//     console.log('task',task);
-//     if (dropZone !== data && !dragElements.includes(dropZone)) {
-//         ev.target.appendChild(draggedElement);
-//     }
-//     console.log(currentTasks);
-// }
-
-
-
-// function drop(ev) {  //ez jo!!
-//   ev.preventDefault();
-//   const data = ev.dataTransfer.getData("text");
-//   const draggedElement = document.getElementById(data);
-//   const dropZone = ev.target.id;
-//   const dragElements = Array.from(document.querySelectorAll('.drag')).map(element => element.id);
-//   const taskId = data;
-//   const taskIndex = currentTasks.findIndex(task => task.id === parseInt(taskId));
-  
-//   if (dropZone !== data && !dragElements.includes(dropZone)) {
-//       // Ändere die Spalte des Tasks
-//       const task = currentTasks[taskIndex];
-//       const newColumn = dropZone;
-//       task.column = newColumn;
-
-//       // Aktualisiere die DOM-Ansicht
-//       const destinationColumn = document.getElementById(newColumn);
-//       destinationColumn.appendChild(draggedElement);
-
-//       // Entferne den Task aus der vorherigen Spalte, wenn vorhanden
-//       const previousColumn = document.getElementById(task.column);
-//       if (previousColumn) {
-//           previousColumn.removeChild(draggedElement);
-//       }
-//   }
-
-//   clearColumn();
-//   loadAllTasks();
-// }
 
 
 function drop(ev) {
@@ -91,64 +41,6 @@ function drop(ev) {
       loadAllTasks();
   }
 }
-
-
-//  function loadAllTasks() {   // ez jo!!!
-  
-//    document.getElementById('layOver').style.display = "none";
-//     currentTasks = tasks;
-//    for (let i = 0; i < currentTasks.length; i++) {
-//      const task = currentTasks[i];
-//      const column = task.column;
-//      const title = task.title;
-//      const id = task.id;
-//      const description = task.description;
-//      const taskIndex = task.taskIndex;
-//      const createdAt = task.createdAt;
-
-//      document.getElementById(column).innerHTML += `
-//      <div class="drag" onclick="openTaskPopUp('${id}','${title}','${column}','${description}','${taskIndex}','${createdAt}')" draggable="true" ondragstart="drag(event)" id=${id}>${title}</div>
-//      ` 
-//     }
-//  }
-
-// function loadAllTasks() {
-//   document.getElementById('layOver').style.display = "none";
-//   currentTasks = tasks;
-  
-//   // Gruppiere Tasks nach Spalten
-//   const tasksByColumn = {};
-//   for (const task of currentTasks) {
-//       if (!tasksByColumn[task.column]) {
-//           tasksByColumn[task.column] = [];
-//       }
-//       tasksByColumn[task.column].push(task);
-//   }
-  
-//   // Rendere Tasks für jede Spalte
-//   for (const column in tasksByColumn) {
-//       if (tasksByColumn.hasOwnProperty(column)) {
-//           // Sortiere Tasks in der aktuellen Spalte nach ihrem taskIndex
-//           const tasksInColumn = tasksByColumn[column].sort((a, b) => a.taskIndex - b.taskIndex);
-          
-//           // Rendere Tasks in der aktuellen Spalte
-//           const columnElement = document.getElementById(column);
-//           columnElement.innerHTML = ''; // Leere Spalte vor dem Rendern
-//           for (const task of tasksInColumn) {
-//               const title = task.title;
-//               const id = task.id;
-//               const description = task.description;
-//               const taskIndex = task.taskIndex;
-//               const createdAt = task.createdAt;
-
-//               columnElement.innerHTML += `
-//                   <div class="drag" onclick="openTaskPopUp('${id}','${title}','${column}','${description}','${taskIndex}','${createdAt}')" draggable="true" ondragstart="drag(event)" id=${id}>${title}</div>
-//               `;
-//           }
-//       }
-//   }
-// }
-
 
 
 function loadAllTasks() {
